a9d37b36999b807935aafbccf9618a9eca18a753
aburov <aburov.spam@gmail.com>
2016-06-07 12:52:19 +0300

    Font fix for page-speed

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -6,7 +6,7 @@
 <?php print $head; ?>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5">
 <?php print $styles; ?>
-<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+<!--<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>-->
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 <?php print $page_top; ?>
